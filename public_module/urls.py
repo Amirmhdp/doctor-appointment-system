64db899f37eb0dc8f8e71f0cb1d8d2fc52f814e1
amir <tavakolianamirmhd@gmail.com>
2026-09-01 21:38:55 +0330

    Contact Us Page
    Designed the Contact Us page UI with contact information, contact form, social links, and location section.

--- public_module/urls.py
+++ public_module/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('about-us', views.AboutUsView.as_view(), name='about_us_page'),
+    path('contact-us', views.ContactUsView.as_view(), name='contact_us_page'),
 ]
--- public_module/views.py
+++ public_module/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 
 class AboutUsView(TemplateView):
     template_name = 'public_module/about_us.html'
+    
+    
+class ContactUsView(TemplateView):
+    template_name = 'public_module/contact_us.html'
